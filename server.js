31ea7e5ef992eb8f270c1da271fd38020ce1fc17
yoni-tad <yonitad2404@gmail.com>
2024-09-02 19:05:48 +0300

    update by  yoni

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import connectDB from "./backend/config/db.js";
-import auth from "./backend/routes/auth.js"
-import books from "./backend/routes/books.js"
-
+import auth from "./backend/routes/auth.js";
+import books from "./backend/routes/books.js";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express(); 
+const coreOptions = {
+  origin: "*",
+  Credential: true,
+  optionSuccessStatus: 200,
+};
 
+const app = express();
 
-app.use(express.json());//to parse json requests
+app.use(express.json()); //to parse json requests
 connectDB();
 
-app.use('/api/auth', auth)
-app.use('/api/books', books)
-app.use(express.static('./frontend'))
+app.use(cors(coreOptions));
+app.use("/api/auth", auth);
+app.use("/api/books", books);
+app.use(express.static("./frontend"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log("Server is listening on port", port));
-
-
